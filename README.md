1cb5372b9de42e4515ce25f80d68087ba5447620
srrrs-7 <srrrs4510@gmail.com>
2025-11-24 05:34:44 +0000

    Docs: Add AWS credentials to README; Chore: Update S3 outputs and terraform lock file

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # play-tf
 
+## AWS credentials
+aws configure
+export AWS_ACCESS_KEY_ID="your-access-key"
+export AWS_SECRET_ACCESS_KEY="your-secret-key"
+export AWS_DEFAULT_REGION="ap-northeast-1"
+
 ## 環境設定
 cd environments/*
 
--- iac/environments/s3/outputs.tf
+++ iac/environments/s3/outputs.tf
@@ -7,18 +7,3 @@ output "app_bucket_arn" {
   description = "Application bucket ARN"
   value       = module.app_bucket.bucket_arn
 }
-
-output "logs_bucket_id" {
-  description = "Logs bucket ID"
-  value       = module.logs_bucket.bucket_id
-}
-
-output "static_bucket_id" {
-  description = "Static content bucket ID"
-  value       = module.static_bucket.bucket_id
-}
-
-output "static_bucket_domain" {
-  description = "Static content bucket domain"
-  value       = module.static_bucket.bucket_domain_name
-}
